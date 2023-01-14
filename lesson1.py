cbf54fb5861bdb1bc1f6288fdb803691c4e593af
Yuriyvas <yuriy.vassiliev2010@gmail.com>
2023-01-15 00:14:35 +0500

    first lesson + homework

--- lesson1.py
+++ lesson1.py
@@ -2,3 +2,10 @@ user_name = input('Enter your name: ')
 message = 'Hello, '
 print(message + user_name)
 print(f'{message}')
+# homework
+print("*************")
+print('*           *')
+print('*           *')
+print("*************")
+
+
