694f1ff4a59405c9d6c0832c1025752fcfc117e4
Yuriyvas <yuriy.vassiliev2010@gmail.com>
2023-01-15 17:06:24 +0500

    first lesson + homework V1.2

--- lesson1.py
+++ lesson1.py
@@ -2,10 +2,19 @@ user_name = input('Enter your name: ')
 message = 'Hello, '
 print(message + user_name)
 print(f'{message}')
-# homework
+
+# homework v1.1
+
 print("*************")
 print('*           *')
 print('*           *')
 print("*************")
+
+# homework v1.2
+
+symbol =  input('Enter any symbol: ')
+symbol2 =  input('Enter any symbol more: ')
+size_pict = int (input('Size:'))
+print(symbol * size_pict + '\n' + symbol + symbol2 * (size_pict -2) +symbol + '\n' + symbol + symbol2 * (size_pict -2) +symbol + '\n' + symbol * size_pict + '\n')
 
 
