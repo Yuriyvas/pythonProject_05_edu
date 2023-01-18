ac84c91496bf5a6e88fd977161f42ffac5a60789
Yuriyvas <yuriy.vassiliev2010@gmail.com>
2023-01-19 01:15:24 +0500

    4th lesson + homework complited + colored

--- Lesson4.py
+++ Lesson4.py
@@ -1,4 +1,9 @@
 import math
+from functools import reduce
+
+
+import my_calc
+
 
 # 4.1. Напишите функцию square, принимающую 1 аргумент — сторону квадрата, и возвращающую 3 значения (с помощью кортежа):
 #      периметр квадрата, площадь квадрата и диагональ квадрата.
@@ -18,16 +23,28 @@ square(int(input('Введите размер стороны квадрата: '
 # 	position: web developer
 def my_dict(**kwargs):
     for k, v in kwargs.items():
-        red="\033[31m{}"
-        blue="\033[34m{}"
-        print(red.format(f'{k}:'), (blue.format(f'{v}:')))
+        red="\033[31m{}".format
+        blue="\033[34m{}".format
+        reset = "\033[0m{}".format('')
+        print(red(f'{k}:'), (blue(f'{v}:')), reset)
+
 
 my_dict(last_name='Popov', name='Max', age=40, position='web developer')
 
 #
 # 4.3. Используя лямбда-выражение, из списка my_list = [20, -3, 15, 2, -1, -21] создайте новый список, содержащий только
 #      положительные числа
+my_list = [20, -3, 15, 2, -1, -21]
+print(list(filter(lambda x: x > 0, my_list)))
+print(reduce(lambda x, y: x*y, my_list))
+
+
 #
 # 4.4. Используя лямбда выражение, получите результат перемножения значений в предыдущем списке
 # 4.5. Создайте файл my_calc.py и пропишите в нем минимум 4 функции, выполняющие базовые арифметические вычисления.
 #      Примените эти функции в качестве методов в другом файле.
+
+print(my_calc.my_sum(6,7))
+print(my_calc.my_minus(6,7))
+print(my_calc.my_umn(6,7))
+print(my_calc.my_stepen(6,7))
---
+++ my_calc.py
@@ -0,0 +1,18 @@
+from functools import reduce
+
+
+# def my_sum():
+# print(my_sum(a))
+    # print(my_sum(b))
+    # print(my_sum(c))
+    # # print(reduce(lambda x, y: x + y, my_sum(a, b, c)))
+def my_sum(x,y):
+   return x+y
+def my_minus(x,y):
+   return x-y
+def my_stepen(x,y):
+   return x**y
+
+def my_umn(x,y):
+   return x*y
+
