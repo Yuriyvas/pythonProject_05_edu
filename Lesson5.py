9a18668d72281566a5cd574c2bc57479523838cb
Yuriyvas <yuriy.vassiliev2010@gmail.com>
2023-01-23 18:00:15 +0500

    6th lesson complete

--- Lesson5.py
+++ Lesson5.py
@@ -10,14 +10,33 @@ class Automobile:
             return 'Wrum-Wrum'
 
 class Bus(Automobile):
-    def __init__(self, brand, model, year, sits):
+    def __init__(self, brand, model, year, sits, __color,__fuel):
         super().__init__(brand, model, year)
         self.sits = sits
+        self.__color = __color
+        self.__fuel = __fuel
+
     def runing(self):
             return 'Tr-tr-tr'
+    def refeul(self, newfuel):
+        self.__fuel = newfuel
+
 
 camry70 = Automobile('Toyota', '70', 2020)
 print(camry70.runing())
 
-hiace = Bus('Toyota', 'hiace', 2015, 16)
+hiace = Bus('Toyota', 'hiace', 2015, 16, 'blue', 'gasoline')
 print(hiace.__dict__)
+try:
+    print(hiace.__fuel)
+except AttributeError: print('No fuel')
+
+hiace.__fuel = 'Oil'
+print(hiace.__fuel)
+print(hiace.__dict__)
+
+Bus.refeul(Bus, 'Mazut')
+print(hiace.__dict__)
+
+couster = Bus('Toyota', 'couster', 2015, 16, 'blue', 'gasoline')
+print(couster.__dict__)
---
+++ Test_login_page.py
@@ -0,0 +1,34 @@
+
+# https://www.saucedemo.com/
+#
+import time
+import pytest
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get('https://www.saucedemo.com')
+def test_login_page():
+    title_from_site = driver.title
+    assert title_from_site =='Swag Labs'
+
+def test_login_form():
+    user_name = driver.find_element(By.CSS_SELECTOR, 'input#user-name')
+    user_name.send_keys('standard_user')
+    time.sleep(2)
+
+    password = driver.find_element(By.CSS_SELECTOR, 'input#password')
+    password.send_keys('secret_sauce')
+    time.sleep(2)
+
+    button_login = driver.find_element(By.CSS_SELECTOR, 'input#login-button')
+    button_login.click()
+    time.sleep(2)
+
+    assert driver.current_url == 'https://www.saucedemo.com/inventory.html', 'Wrong site!'
+
+    driver.quit()
+
+
+
